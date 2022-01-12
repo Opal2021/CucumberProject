78f555a91daf2bb4a6e640bf4d2e733f1110a770
Hannah <84111652+HannahHannah3@users.noreply.github.com>
2022-01-11 20:24:20 -0500

    checking

--- src/test/java/pagesClass/CardioPage.java
+++ src/test/java/pagesClass/CardioPage.java
@@ -1,5 +1,18 @@
 package pagesClass;
 
-public class CardioPage {
+import org.openqa.selenium.support.PageFactory;
 
+import utilities.Driver;
+
+public class CardioPage {
+	
+	public CardioPage(){
+		
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+
+	
+	//@FindBy()
+	
+	
 }
