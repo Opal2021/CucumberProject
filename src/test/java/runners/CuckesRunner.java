9bf8dea4cee966dbfeb7b9b75c7feb2d7a24f57d
ZamiraM0207 <88807691+ZamiraM0207@users.noreply.github.com>
2022-01-13 22:11:05 -0500

    zamira code

--- src/test/java/runners/CuckesRunner.java
+++ src/test/java/runners/CuckesRunner.java
@@ -16,8 +16,8 @@ import cucumber.api.junit.Cucumber;
 		},
 		features = "src/test/resources/feature/",
 		glue     = "stepDefinitions",
-		dryRun   = false,
-		tags     = "@tag1" //"@SmokeTest"
+		dryRun   = true
+		//tags     = "@tag1" //"@SmokeTest"
 
 				
 )
