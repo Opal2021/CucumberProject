e87c3406471d974b5602081f0600e8ce271513e4
ZamiraM0207 <88807691+ZamiraM0207@users.noreply.github.com>
2022-01-14 19:15:28 -0500

    zamira's change

--- src/test/java/pagesClass/LogInPage.java
+++ src/test/java/pagesClass/LogInPage.java
@@ -8,40 +8,40 @@ import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class LogInPage {
-	
+
 	public LogInPage() {
 
 		PageFactory.initElements(Driver.getDriver(), this);
 	}
-	@FindBy(xpath="//*[@id=\"header\"]/div[4]/a[1]")
-	
+
+	@FindBy(xpath = "//*[@id=\"header\"]/div[4]/a[1]")
+
 	public WebElement LoginTab;
-	
-	@FindBy(id="login-username")
-	
+
+	@FindBy(id = "login-username")
+
 	public WebElement EmailFields;
-	
-	@FindBy(id="login-password")
-	
+
+	@FindBy(id = "login-password")
+
 	public WebElement PasswordFields;
-	
-	@FindBy(id="login-button")
-	
+
+	@FindBy(id = "login-button")
+
 	public WebElement LoginButton;
 
-@FindBy (xpath="//span[@class='welcome-message']")
-	
+	@FindBy(xpath = "//span[@class='welcome-message']")
+
 	public WebElement WelcomeMessage;
 
+	public void LogIn_ccb_200() {
+		
 
-public void LogIn_ccb_200 (){
-	LoginTab.click();
-	EmailFields.sendKeys(ConfigurationReader.getProperty("Email_200"));
-	PasswordFields.sendKeys(ConfigurationReader.getProperty("Password_200"));
-	LoginButton.click();
-	
-	
-	
-}
+		LoginTab.click();
+		EmailFields.sendKeys(ConfigurationReader.getProperty("Email_200"));
+		PasswordFields.sendKeys(ConfigurationReader.getProperty("Password_200"));
+		LoginButton.click();
+
+	}
 
 }
