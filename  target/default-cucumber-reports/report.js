1e600acc73143c4f952700d93f8e18cf06182545
Opal2021 <88755505+Opal2021@users.noreply.github.com>
2022-01-19 17:24:18 -0500

    test

---  target/default-cucumber-reports/report.js
+++  target/default-cucumber-reports/report.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/feature/CCB_500_eBikesFunctionality.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/feature/CCB_300_MyAccountFunctionality.feature");
 formatter.feature({
-  "name": "Verify eBikes Functionality",
+  "name": "Verify function and data on MyAccount tab",
   "description": "",
   "keyword": "Feature",
   "tags": [
     {
-      "name": "@SmokeTestNadia"
+      "name": "@Opal"
     }
   ]
 });
@@ -18,51 +18,51 @@ formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "User navigates to ebikes page",
+  "name": "A user is able to login with a valid username/password",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_navigates_to_ebikes_page()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_is_able_to_login_with_a_valid_username_password()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Verify user can add item to cart on eBikes page",
+  "name": "Verify the  information in Billing session that successfully save",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@SmokeTestNadia"
+      "name": "@Opal"
     }
   ]
 });
 formatter.step({
-  "name": "User clicks on Add to cart button",
+  "name": "A user navigates to My Account tab",
   "keyword": "When "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_clicks_on_Add_to_cart_button()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_navigates_to_My_Account_tab()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User clicks on Checkout button",
+  "name": "A user is able to change the info in address fields box to \"5678\"",
   "keyword": "And "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_clicks_on_Checkout_button()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_is_able_to_change_the_info_in_address_fields_box_to(String)"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User is on Shopping cart page",
+  "name": "A user should successfully verify a message Your details has been save",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_is_on_Shopping_cart_page()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_should_successfully_verify_a_message_Your_details_has_been_save()"
 });
 formatter.result({
   "status": "passed"
@@ -79,71 +79,41 @@ formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "User navigates to ebikes page",
+  "name": "A user is able to login with a valid username/password",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_navigates_to_ebikes_page()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_is_able_to_login_with_a_valid_username_password()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Verify user can delete item from shopping cart",
+  "name": "Verify radio button is selected in Filter session on Web Orders Tap.",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@SmokeTestNadia"
+      "name": "@Opal"
     }
   ]
 });
 formatter.step({
-  "name": "User clicks on Add to cart button",
+  "name": "A user is navigate to Orders Tab",
   "keyword": "When "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_clicks_on_Add_to_cart_button()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_is_navigate_to_Orders_Tab()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User clicks on Checkout button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_clicks_on_Checkout_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User removes item from Shopping cart",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_removes_item_from_Shopping_cart()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User accepts alert message",
+  "name": "A user is able to verify if Order Number is selected",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_accepts_alert_message()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Shopping cart is empty",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "CCB_500_EbikesFunctionality.shopping_cart_is_empty()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_is_able_to_verify_if_Order_Number_is_selected()"
 });
 formatter.result({
   "status": "passed"
@@ -160,41 +130,51 @@ formatter.before({
   "status": "passed"
 });
 formatter.step({
-  "name": "User navigates to ebikes page",
+  "name": "A user is able to login with a valid username/password",
   "keyword": "Given "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_navigates_to_ebikes_page()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_is_able_to_login_with_a_valid_username_password()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.scenario({
-  "name": "Verify user cannot add to cart out-of-stock items",
+  "name": "Verify adding favorite part number on Favorites Tap successfully.",
   "description": "",
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@SmokeTestNadia"
+      "name": "@Opal"
     }
   ]
 });
 formatter.step({
-  "name": "User selects out of stock item",
+  "name": "A user is navigates to Favorite Tab.",
   "keyword": "When "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_selects_out_of_stock_item()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_is_navigates_to_Favorite_Tab()"
 });
 formatter.result({
   "status": "passed"
 });
 formatter.step({
-  "name": "User cannot see add to card button",
+  "name": "A user enters part number in the fields.",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "CCB_300_MyAccountFunctionality.a_user_enters_part_number_in_the_fields()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "A user successfully verifies a message Product Added Successfully",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "CCB_500_EbikesFunctionality.user_cannot_see_add_to_card_button()"
+  "location": "CCB_300_MyAccountFunctionality.a_user_successfully_verifies_a_message_Product_Added_Successfully()"
 });
 formatter.result({
   "status": "passed"
