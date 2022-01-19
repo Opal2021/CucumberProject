55722b8d81b98b7a971b3ea81b8c623aca8f6a18
Nadi8 <nadia.erdinieva@gmail.com>
2022-01-19 18:42:36 -0500

    updated cuckes

--- src/test/java/runners/CuckesRunner.java
+++ src/test/java/runners/CuckesRunner.java
@@ -16,8 +16,8 @@ import cucumber.api.junit.Cucumber;
 		},
 		features = "src/test/resources/feature/",
 		glue     = "stepDefinitions",
-		dryRun   = false,
-		tags     ="@SmokeTest50"//"@Opal"//"@SmokeTestNadia"//"@tag200"//"@SmokeTest4"//"@tag10"
+		dryRun   = false
+//		tags     =//"@SmokeTest50"//"@Opal"//"@SmokeTestNadia"//"@tag200"//"@SmokeTest4"//"@tag10"
 
 				
 )
